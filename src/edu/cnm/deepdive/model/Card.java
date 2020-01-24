390495c483ba5441ad2715c2fd8e598e9ae9f1ff
Steven Zuniga <szuniga@cnm.edu>
2020-01-24 09:38:17 -0700

    Added a card comparison machinery to prepare for test.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,13 +1,17 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash( suit, rank );
   }
 
   public Suit getSuit() {
@@ -21,6 +25,27 @@ public class Card {
   @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
+  }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+
+    boolean comparison = false;
+    if (obj == this) {
+      comparison = true;
+    } else if (obj instanceof Card && obj.hashCode() == hash) {
+      Card other = (Card) obj;
+      if (suit == other.suit && rank == other.rank) {
+        comparison = true;
+      }
+    }
+
+    return comparison;
   }
 }
 
