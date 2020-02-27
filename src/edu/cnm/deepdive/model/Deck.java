09a3df2cd80c6f8d7091726c6b2a59ba3f82d5f3
Steven Zuniga <szuniga@cnm.edu>
2020-02-27 10:09:50 -0700

    Added sort method with comparator lambda.

--- src/edu/cnm/deepdive/model/Deck.java
+++ src/edu/cnm/deepdive/model/Deck.java
@@ -7,11 +7,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The class encapsulates the deck method.
+ */
 public class Deck {
 
   private List<Card> cards;
   private List<Card> dealt;
 
+  /**
+   * The methods define cards and dealt with a list function and assigns a value of s and r.
+   */
   public Deck() {
     cards = new ArrayList<>();
     dealt = new LinkedList<>();
@@ -22,6 +28,10 @@ public class Deck {
     }
   }
 
+  /**
+   * Returns card instance value that shows that the cards have been dealt.
+   * @return
+   */
   public Card deal() {
     Card card = cards.isEmpty() ? null : cards.remove(0);
     if (card != null) {
@@ -30,16 +40,32 @@ public class Deck {
     return card;
   }
 
+  /**
+   * Initializes the shuffle method using a rng.
+   * @param rng
+   */
   public void shuffle(Random rng) {
-    cards.addAll(dealt);
-    dealt.clear();
+    gather();
     Collections.shuffle(cards, rng);
   }
 
+  private void gather() {
+    cards.addAll(dealt);
+    dealt.clear();
+  }
+
+  /**
+   * Gives a return value that counts the remaining cards left in the deck.
+   * @return
+   */
   public int remaining() {
     return cards.size();
   }
 
+  /**
+   * Gives a return count using int of the size of the deck of cards that have been dealt.
+   * @return
+   */
   public int dealt() {
     return dealt.size();
   }
@@ -47,6 +73,19 @@ public class Deck {
   @Override
   public String toString() {
     return cards.toString();
+  }
+
+  public void sort(boolean gather) {
+    if(gather) {
+      gather();
+    }
+    cards.sort((card1, card2) -> {
+      int result = card1.getSuit().compareTo(card2.getSuit());
+      if (result == 0) {
+        result = card1.getRank().compareTo(card2.getRank());
+      }
+      return result;
+    } );
   }
 
 }
